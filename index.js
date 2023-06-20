128d903ce449aa134f99bae7d892b879b2f5dce2
Fabian Velarde <fabianikv@gmail.com>
2023-06-20 13:08:36 -0300

    se corrige preEntrega y se agrega prompt

---
+++ index.js
@@ -0,0 +1,27 @@
+// Algoritmo con estructura condicional
+//se corrige Prompt
+
+let edad = prompt("por favor, ingresa tu edad");
+
+if (edad >= 18) {
+    alert("Sos mayor ingresa");
+} else{
+    alert("sos menor, no podes ingresar.");
+}
+
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+
+//determina si un numero es par o impar
+
+for ( let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+    console.log(i + "es par");
+    } else {
+        console.log(i + "es impar");
+    }
+}
